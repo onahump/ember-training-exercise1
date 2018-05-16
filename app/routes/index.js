1a55847b4c2b650f8a18d01d873009b4368dc6a3
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-05-15 21:11:26 -0500

    Linking index with page1, page2 and page3 using an action in button

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+	model(){
+
+	},
+
+  	actions:{
+	
+		clickMeToPage1:function(){
+			location.replace("page1")
+		},
+
+		clickMeToPage2:function(){
+			location.replace("page2")
+		},
+
+		clickMeToPage3:function(){
+			location.replace("page3")
+		}
+	
+	}
 });
